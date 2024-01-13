3563912efea50b3c5e83323acfd117a232ebc24f
Andrii <andriikuznichenko@gmail.com>
2024-01-13 17:19:44 +0100

    width of the homepage

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -137,10 +137,10 @@ const handleSubmit = e => {
                   </div>
                   <div className='mt-3'>
                     <button type="button" 
-                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                     onClick={() => setDeleteConfirmation(!deleteConfirmation)}>Delete</button>
                     <button type="button" 
-                    class="focus:outline-none text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-orange-900"
+                    className="focus:outline-none text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-orange-900"
                     >Update</button>
                 </div>
                 </div>
